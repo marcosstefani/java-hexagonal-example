75fc5ca80b22e4ef84e4d3144f353e0cadd6ec87
Marcos Rosa <marcos.rosa@mindera.com>
2021-06-19 13:12:04 -0300

    my first commit

---
+++ src/test/java/io/github/marcosstefani/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marcosstefani.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
